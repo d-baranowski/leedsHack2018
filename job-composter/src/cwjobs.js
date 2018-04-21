578df995612c42a9e823144e8b895b9b4331a3b5
Pedro Borges <pedro.borges@itv.com>
2018-04-21 21:39:47 +0100

    loade impl

--- job-composter/src/cwjobs.js
+++ job-composter/src/cwjobs.js
@@ -1,17 +1,19 @@
 import data from "./data";
-import {waitForElement} from "./common";
+import {waitForElement, sendToAggregator} from "./common";
 
 window.onload = () => {
-   console.log("Window load debug");
-   const promise = waitForElement(".job-description");
-   promise.then(() => {
-       console.log("element found");
-       const advertText = document.querySelector('.job-description').textContent;
+    console.log("Window load debug");
+    const promise = waitForElement(".job-description");
+    promise.then(() => {
+        console.log("element found");
+        const advertText = document.querySelector('.job-description').textContent;
 
-           console.log("analysing element ", advertText);
-           const foundIt = data.getReplacements(advertText);
-           if (foundIt) {
-               console.log(foundIt);
-           }
-   });
+            console.log("analysing element ", advertText);
+            const foundIt = data.getReplacements(advertText);
+            if (foundIt) {
+                console.log(foundIt);
+                const body = JSON.stringify({"keywords" : foundIt, "url" : document.URL})
+                sendToAggregator(body)
+            }
+    });
 };
