e15e2bfe7da754974f2a03d9558c9be635c3acb4
William Heng <william@williamheng.com>
2018-04-21 20:24:45 +0100

    Bullshit Generator: Generate bullshit from text file and POSTs them to remote database

---
+++ bullshit-generator/src/main/kotlin/com/c0ff33/Generator.kt
@@ -0,0 +1,45 @@
+package com.c0ff33
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClients
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class Generator
+
+fun main(args: Array<String>) {
+
+    val url = "https://9rph5cqv47.execute-api.eu-west-2.amazonaws.com/dev/replacements"
+    val objectMapper = ObjectMapper()
+    val httpClient = HttpClients.createDefault()
+
+    val resource = Generator::class.java.classLoader.getResource("input.txt")
+    val path = Paths.get(resource.toURI())
+    val stream = Files.lines(path)
+
+    stream.map { line ->
+        val components = line.split("=")
+        val textPattern = components[0].trim()
+        val replacementTexts = components[1].split("/").map { s -> s.trim() }
+
+        Replacement(textPattern, replacementTexts)
+    }.forEach({ replacement ->
+
+        val httpPost = HttpPost(url)
+        val requestEntity = StringEntity(objectMapper.writeValueAsString(replacement))
+        httpPost.entity = requestEntity
+        httpPost.setHeader("Content-type", "application/json")
+        httpClient.execute(httpPost)
+
+    })
+
+
+//    val file = File("output.json")
+//
+//    objectMapper.writeValue(file.outputStream(), replacements)
+
+}
+
+data class Replacement(val textPattern: String, val replacementTexts: List<String>)
